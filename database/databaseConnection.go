be68271e35140b14aa1da116877fd3ca81e14811
Victory Anusie <victoryanusie@gmail.com>
2024-08-28 16:07:40 +0100

    new

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -7,37 +7,37 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func DBInstance() *mongo.Client{
-	MongoDb := "mongodb://localhost:2701"
+func DBinstance() *mongo.Client{
+	MongoDb := "mongodb://localhost:27017"
 	fmt.Print(MongoDb)
 
-
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
-	if err != nil{
+
+	if err!= nil {
 		log.Fatal(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
 	defer cancel()
 
 	err = client.Connect(ctx)
-	if err != nil {
+
+	if err!= nil{
 		log.Fatal(err)
 	}
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to MongoBD!")
-	return client 
+	fmt.Println("connected to mongodb")
+	return client
 }
 
-var Client *mongo.Client = DBInstance()
+
+var Client *mongo.Client = DBinstance()
+
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection{
-	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+	var collection *mongo.Collection = client.Database("vicDatabase").Collection(collectionName)
 
 	return collection
 }
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -8,7 +8,7 @@ import (
 
 
 func UserRoutes(incomingRoutes *gin.Engine){
-	incomingRoutes.GET("/users", controller.GetUser())
+	incomingRoutes.GET("/users", controller.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controller.GetUser())
 	incomingRoutes.POST("/users/singup", controller.SingUp)
 	incomingRoutes.POST("/users/login", controller.Login())
