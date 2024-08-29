d4e3d90ce30a1674b707abe9763d985a3092c9e6
Victory Anusie <victoryanusie@gmail.com>
2024-08-29 15:51:11 +0100

    Food Controller Completed

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -14,7 +14,9 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 var validate = validator.New()
@@ -47,7 +49,7 @@ func GetFoods() gin.HandlerFunc {
         groupStage := bson.D{
             {Key: "$group", Value: bson.D{
                 {Key: "_id", Value: nil}, // Grouping without a specific field to count all
-                {Key: "total_count", Value: bson.D{{Key: "$sum", Value: 1}}}, // Summing 1 for each document
+                {Key: "total_count", Value: bson.D{{Key: "$sum", Value: 1}}}, {Key: "data", Value: bson.D{{Key: "$push", Value: "$$ROOT"}}},
             }},
         }
 
@@ -64,24 +66,21 @@ func GetFoods() gin.HandlerFunc {
         pipeline := mongo.Pipeline{matchStage, groupStage, projectStage}
 
         // Execute the aggregation query
-        cursor, err := foodCollection.Aggregate(ctx, pipeline)
+        result, err := foodCollection.Aggregate(ctx, pipeline)
         if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to execute query: " + err.Error()})
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to execute query:"})
             return
         }
 
-        var results []bson.M
-        if err := cursor.All(ctx, &results); err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode results: " + err.Error()})
+        var allFoods []bson.M
+        if err := result.All(ctx, &allFoods); err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode results"})
             return
         }
 
-        c.JSON(http.StatusOK, results)
+        c.JSON(http.StatusOK, allFoods[0])
     }
 }
-
-
-
 
 func GetFood() gin.HandlerFunc{
 	return func(c *gin.Context){
@@ -142,15 +141,66 @@ func CreateFood() gin.HandlerFunc{
 }
 
 func round(num float64) int {
-
+	return int(num + math.Copysign(0.5, num))
 }
 
 func toFixed(num float64, precision int) float64{
-	
+	output := mat.Pow(10, float64(precision))
+	return float64(round(num*output)) /  output
 }
 
 func UpdateFood() gin.HandlerFunc{
 	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var menu models.Menu
+		var food models.Food
+		foodID := c.Param("food_id")
+
+		if err := c.BindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var updateObj primitve.D{}
+
+		if food.Name != nil {
+			updateObj = append(updateObj, bson.E{Key: "name", Value: food.Name})
+		}
+
+		if food.Price != nil {
+			updateObj = append(updateObj, bson.E{Key: "food_price", Value: food.Price})
+		}
+
+		if food.Food_image != nil {
+			updateObj = append(updateObj, bson.E{Key: "food_image", Value: food.Food_image})
+		}
+
+		if food.Menu_id != nil {
+			err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+			defer cancel()
+			if err!= nil{
+				msg := fmt.Sprintf("message: Menu was not found")
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+			updateObj = append(updateObj, bson.E{Key: "menu", Value: food.Price})
+		}
+		
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{Key: "updated_at", Value: food.Updated_at})
+
+		
+		filter := bson.M{"food_id": foodID}
+
+		upsert := true
+        opts := options.UpdateOptions{Upsert: &upsert}
+
+		result, err := foodCollection.UpdateOne(ctx, filter, bson.D{{Key: "$set", Value: updateObj}}, &opts)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+            return
+        }
+		c.JSON(http.StatusOK, result)
 	}
 }
