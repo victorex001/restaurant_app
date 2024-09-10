19d9808d8b9c00a1aa6b1bdfd4e6700139472e76
Victory Anusie <victoryanusie@gmail.com>
2024-09-10 22:19:39 +0100

    jwt

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,7 +1,20 @@
 package helpers
 
 import (
-	"github.com/dgrijalva/jwt-go" // Corrected import path
+	"context"
+	"log"
+	"os"
+	"restaurant_app/database"
+	"time"
+
+	// jwt "github.com/dgrijalva/jwt-go" // Corrected import path
+	// "github.com/golang-jwt/jwt"
+	// "github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -12,12 +25,77 @@ type SignedDetails struct {
 	jwt.StandardClaims
 }
 
-func GenerateAllTokens(){
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+
+func GenerateAllTokens(email, firstName, lastName, uid string) (signedToken string, signedRefreshToken string, err error) {
+    // Setup claims for the access token
+    claims := &SignedDetails{
+        Email:      email,
+        FirstName:  firstName,
+        LastName:   lastName,
+        Uid:        uid,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Add(15 * time.Minute).Unix(), // 15 minutes for access token
+        },
+    }
+
+    // Setup claims for the refresh token
+    refreshClaims := &SignedDetails{
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Add(7 * 24 * time.Hour).Unix(), // 7 days for refresh token
+        },
+    }
+
+    // Generate the access token
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    signedToken, err = token.SignedString([]byte(SECRET_KEY))
+    if err != nil {
+        log.Printf("Failed to sign the access token: %v", err)
+        return
+    }
+
+    // Generate the refresh token
+    refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+    signedRefreshToken, err = refreshToken.SignedString([]byte(SECRET_KEY))
+    if err != nil {
+        log.Printf("Failed to sign the refresh token: %v", err)
+        return
+    }
+    return signedToken, signedRefreshToken, nil
 }
 
-func UpdateAllToken(){
 
+func UpdateAllToken(signedToken string, signedRefreshToken string, userId string){
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{Key: "token", Value: signedToken})
+	updateObj = append(updateObj, bson.E{Key: "refresh_token", Value: signedRefreshToken})
+
+	Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{Key: "updated_at", Value: Updated_at})
+	
+
+	filter := bson.M{"user_id": userId}
+	upsert := true
+    opts := options.UpdateOptions{Upsert: &upsert}
+
+
+	_, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{{Key: "$set", Value: updateObj}},
+		&opts,
+	)
+	if err != nil {
+		log.Printf("Failed to update tokens: %v", err)
+        return
+	}
 }
 
 
